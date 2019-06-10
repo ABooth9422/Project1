2c6fa04314267c52a4f2191d4d76840117c15874
Jester <ABooth9422@gmail.com>
2019-06-10 16:44:00 -0400

    just need to polish

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-
+//using firebase in the code to store variables that we are getting from the api
 var firebaseConfig = {
     apiKey: "AIzaSyBh9Wg4jgKRJrMgmm3c-qZJeYbFeLKuiog",
     authDomain: "tattoo4u-1.firebaseapp.com",
@@ -19,6 +19,7 @@ var database = firebase.database();
 
 
 function pull(cityVal, tattooStyle) {
+    // yelp url and api call for searching tattoo shops in the area specifically three
     var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=tattoos&location=" + cityVal;
 
     $.ajax({
@@ -29,6 +30,8 @@ function pull(cityVal, tattooStyle) {
         url: queryURL,
 
         method: "GET"
+         //using GET method
+        // call a method "then" for function to use as the callbacks
     }).then(function (response) {
         var businessArray = response.businesses;
         var styleArray = ["newSchool", "traditional", "realism", "script", "watercolor"]
@@ -42,98 +45,128 @@ function pull(cityVal, tattooStyle) {
                 ratings: businessArray[i].rating,
                 reviewCount: businessArray[i].review_count,
                 thumbnail: businessArray[i].image_url,
-                address:businessArray[i].location.display_address,
-                latitude:businessArray[i].coordinates.latitude,
-                longitude:businessArray[i].coordinates.longitude,
+                address: businessArray[i].location.display_address,
+                latitude: businessArray[i].coordinates.latitude,
+                longitude: businessArray[i].coordinates.longitude,
                 style: style
             })
-            
 
+             //using business array and forloop to attain a random amount of business from three different areas
+            //also pulling location name of business phone rating and review count, etc..
 
 
         }
         database.ref().on("child_added", function (snapshot) {
+            //when a child is added we are checking to see if it matches the users choice of style
+            //if it does we are dynamically creating all of these elements on the page
             var sv = snapshot.val();
             console.log(sv)
             if (sv.style === tattooStyle) {
-                var row=$("<div>")
+                var row = $("<div>")
                 row.addClass("container")
                 row.addClass("my-5")
                 row.addClass("results")
-                var image=$("<img src= still image'>")
+                var image = $("<img src= still image'>")
                 image.addClass("img-thumbnail")
                 image.attr("src", sv.thumbnail)
-                var ul=$("<ul>")
+                var ul = $("<ul>")
                 ul.addClass("mx-5")
                 var name = $("<h5>")
-                name.text("Shop Name: "+sv.name)
-                var address=$("<h5>")
+                name.text("Shop Name: " + sv.name)
+                var address = $("<h5>")
                 address.text(sv.address[0])
-                var address2=$("<h5>")
+                var address2 = $("<h5>")
                 address2.text(sv.address[1])
-                var phone=$("<h5>")
-                phone.text("Phone: "+sv.phone)
-                var rating=$("<h5>")
-                rating.text("Rating: "+ sv.ratings)
-                var reviewCount=$("<h5>")
-                reviewCount.text("Reviews: " +sv.reviewCount)
-                var button=$("<button>")
+                var phone = $("<h5>")
+                phone.text("Phone: " + sv.phone)
+                var rating = $("<h5>")
+                rating.text("Rating: " + sv.ratings)
+                var reviewCount = $("<h5>")
+                reviewCount.text("Reviews: " + sv.reviewCount)
+                var button = $("<button>")
                 button.addClass("btn btn-secondary")
                 button.addClass("resultButton")
-                button.attr("id",sv.name)
-                button.attr("data-target",".moreImages")
-                button.attr("data-toggle","modal")
-                button.text("click for more pictures!")
-                $(".resultButton").on("click",function(){
-                    moreResults(sv.id)
-                })
-                var mapButton=$("<button>")
-                mapButton.attr("id","clickMap")
-                mapButton.addClass("btn btn-secondary mx-2")
-                mapButton.attr("data-target",".moreMap")
-                mapButton.attr("data-toggle","modal")
-                mapButton.text("Click for the Map")
-                $("#clickMap").on("click",function(){
-                    console.log(sv.latitude)
-                    console.log(sv.longitude)
-                    initMap(sv.latitude,sv.longitude)
-                })
 
-           
-                ul.append(name,address,address2,phone,rating,reviewCount,button,mapButton)
-                row.append(image,ul)
+                button.data("id", sv.id)
+                button.attr("data-target", ".moreImages")
+                button.attr("data-toggle", "modal")
+                button.text("click for more pictures!")
+
+                var mapButton = $("<button>")
+                mapButton.addClass("clickMap")
+                mapButton.addClass("btn btn-secondary mx-2")
+                mapButton.attr("data-target", ".moreImages")
+                mapButton.attr("data-toggle", "modal")
+                mapButton.text("Click for the Map")
+                mapButton.data("latitude", sv.latitude)
+                mapButton.data("longitude", sv.longitude)
+
+                  //using child added and snapshot fucntion to retrieve business names, reviews, pictures through pressing buttons
+                //added buttons to get map location and click for map and added variables to place tattoo shop information on page
+
+                ul.append(name, address, address2, phone, rating, reviewCount, button, mapButton)
+                row.append(image, ul)
                 row.appendTo("#resultsDiv")
             }
         })
     })
 }
+//on click function for the resultButton
+$(document).on("click", ".resultButton", function () {
+    //we are getting the id from the button we stored the value i
+    console.log($(this))
+    var id = $(this).data("id")
+    console.log(id)
+    moreResults(id)
+})
+//on click function for the clickMap function
+$(document).on("click", ".clickMap", function () {
+    //we are getting the values from the button that we stored
+    var longitude = $(this).data("longitude")
+    console.log(longitude)
+    var latitude = $(this).data("latitude")
+    console.log(latitude)
+    initMap(latitude, longitude)
 
-function moreResults(id){
-    
+})
+
+function moreResults(id) {
+
     let id1 = id;
     var queryId1 = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + id1;
-    
+    $("#modalRow").empty();
+    //ajax call for yelp authorization key and tattoo businesses in area
     $.ajax({
-    
-      headers: {
+
+        headers: {
             Authorization: "BEARER pggyYPUFjKRZwKGVB3XiuwMO0wrXgQxau8y3DZW7geuRWY4AgHMklAati700_uZcpaX7LA92bSIxf8YgoYZpI4VBB1dVtoGUgTivNlASsTnoL8Nr1ZxNM90NNSP1XHYx",
         },
         url: queryId1,
-    
+
         method: "GET"
     }).then(function (response) {
-            console.log(response)
-            for (let index = 0; index < response.photos.length; index++) {
-            var moreImages=$("<img src= still image'>")
-            moreImages.attr("src",response.photos[index])
+        console.log(response)
+        // variable more images to get different images from the yelp search
+        for (let index = 0; index < response.photos.length; index++) {
+            var moreImages = $("<img src= still image'>")
+            moreImages.attr("src", response.photos[index])
             moreImages.addClass("img-thumbnail")
             moreImages.addClass("mx-2 my-2")
             moreImages.appendTo($("#modalRow"))
-            }
         }
-    );
-    
+    });
+
 }
-function initMap(latitude,longitude) {
-   // https://maps.googleapis.com/maps/api/staticmap?center=40.714%2c%20-73.998&zoom=12&size=400x400&key=YOUR_API_KEY
+//a function used to display the map in the modal
+function initMap(latitude, longitude) {
+   console.log(latitude)
+   console.log(longitude)
+   $("#modalRow").empty();
+  
+    var address = "https://www.mapquestapi.com/staticmap/v5/map?key=loorHzIG2sew2tOMV3HYNMxhCMwd8i8i&locations="+latitude+","+longitude+"&size=400,400"
+        
+        var map=$("<img src= still image'>")
+        map.attr("src",address)
+        map.appendTo($("#modalRow"))
+    
 }
