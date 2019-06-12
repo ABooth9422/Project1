fd79b4a1ac57a5add4bde35443e08c5d3d4f5bb1
Jester <ABooth9422@gmail.com>
2019-06-11 20:41:39 -0400

    final product?

--- logic.js
+++ logic.js
@@ -25,11 +25,11 @@ function pull(cityVal, tattooStyle) {
         url: queryURL,
 
         method: "GET"
-         //using GET method
+        //using GET method
         // call a method "then" for function to use as the callbacks
     }).then(function (response) {
         var businessArray = response.businesses;
-        var styleArray = ["newSchool", "traditional", "realism", "script", "watercolor"]
+        var styleArray = ["newSchool", "traditional", "realism", "script", "watercolor","japanese","geometric"]
         console.log(response)
         for (let i = 0; i < businessArray.length; i++) {
 
@@ -47,14 +47,14 @@ function pull(cityVal, tattooStyle) {
                 style: style
             })
 
-             //using business array and forloop to attain a random amount of business from three different areas
+            //using business array and forloop to attain a random amount of business from three different areas
             //also pulling location name of business phone rating and review count, etc..
 
 
         }
-       
+
         rowExplanation();
-     
+
 
         database.ref().on("child_added", function (snapshot) {
             //when a child is added we are checking to see if it matches the users choice of style
@@ -77,32 +77,33 @@ $(document).on("click", ".resultButton", function () {
 })
 $(document).on("click", ".imgModal", function () {
     //we are getting the id from the button we stored the value i
-   
+
     var id = $(this).data("id")
-    
+
     moreResults(id)
 })
 //on click function for the clickMap function
-$(document).on("click", ".clickMap", function (latitude,longitude) {
+$(document).on("click", ".clickMap", function (latitude, longitude) {
     //we are getting the values from the button that we stored
     var longitude = $(this).data("longitude")
-    
+
     var latitude = $(this).data("latitude")
     initMap(latitude, longitude)
 
 })
 
-$(document).on("click",".clickReview",function(id){
+$(document).on("click", ".clickReview", function (id) {
 
-    var id=$(this).data("id")
+    var id = $(this).data("id")
     reviewContent(id);
 
 })
-function reviewContent(id){
+
+function reviewContent(id) {
     let id1 = id;
-    var queryId1 = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/"+id1+"/reviews";
+    var queryId1 = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + id1 + "/reviews";
     $("#modalRow").empty();
-    
+
     $.ajax({
 
         headers: {
@@ -114,24 +115,25 @@ function reviewContent(id){
     }).then(function (response) {
         console.log(response)
         for (let i = 0; i < response.reviews.length; i++) {
-        var title=$("<h5>")
-        title.addClass("row")
-        title.addClass("text-center")
-        title.addClass("d-flex justify-content-center")
-        title.addClass("text-white")
-        title.text(response.reviews[i].time_created)
-        var context=$("<p>")
-        context.addClass("row")
-        context.addClass("text-center")
-        context.addClass("p-5")
-        context.addClass("text-white")
-        context.text(response.reviews[i].text)
-        title.appendTo($("#modalRow"))
-        context.appendTo($("#modalRow"))
-        
+            var title = $("<h5>")
+            title.addClass("row")
+            title.addClass("text-center")
+            title.addClass("d-flex justify-content-center")
+            title.addClass("text-white")
+            title.text(response.reviews[i].time_created)
+            var context = $("<p>")
+            context.addClass("row")
+            context.addClass("text-center")
+            context.addClass("p-5")
+            context.addClass("text-white")
+            context.text(response.reviews[i].text)
+            title.appendTo($("#modalRow"))
+            context.appendTo($("#modalRow"))
+
         }
-})
+    })
 }
+
 function moreResults(id) {
     let id1 = id;
     var queryId1 = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + id1;
@@ -160,19 +162,20 @@ function moreResults(id) {
 }
 //function used to display the map in the modal
 function initMap(latitude, longitude) {
-   console.log(latitude)
-   console.log(longitude)
-   $("#modalRow").empty();
-  
-    var address = "https://www.mapquestapi.com/staticmap/v5/map?key=loorHzIG2sew2tOMV3HYNMxhCMwd8i8i&locations="+latitude+","+longitude+"&zoom=14&size=400,400"
-        
-        var map=$("<img src= still image'>")
-        map.attr("src",address)
-        map.addClass("my-5")
-        map.appendTo($("#modalRow"))
-    
+    console.log(latitude)
+    console.log(longitude)
+    $("#modalRow").empty();
+
+    var address = "https://www.mapquestapi.com/staticmap/v5/map?key=loorHzIG2sew2tOMV3HYNMxhCMwd8i8i&locations=" + latitude + "," + longitude + "&zoom=15&size=400,400&type=hyb"
+
+    var map = $("<img src= still image'>")
+    map.attr("src", address)
+    map.addClass("my-5")
+    map.appendTo($("#modalRow"))
+
 }
-function createContent(sv){
+
+function createContent(sv) {
     var row = $("<div>")
     row.addClass("container")
     row.addClass("my-5")
@@ -180,7 +183,7 @@ function createContent(sv){
     var image = $("<img src= still image'>")
     image.addClass("img-thumbnail")
     image.addClass("imgModal")
-   
+
     image.attr("src", sv.thumbnail)
     image.attr("data-target", ".moreImages")
     image.attr("data-toggle", "modal")
@@ -188,17 +191,18 @@ function createContent(sv){
     imageError();
     var ul = $("<ul>")
     ul.addClass("mx-5")
+    ul.addClass("rounded p-2")
     var name = $("<h5>")
     name.text("Shop Name: " + sv.name)
-    var stars=$("<img src= still image'>")
-    reviewStars(sv,stars);
+    var stars = $("<img src= still image'>")
+    reviewStars(sv, stars);
     var address = $("<h5>")
     address.text(sv.address[0])
     var address2 = $("<h5>")
     address2.text(sv.address[1])
     var phone = $("<h5>")
     phone.text("Phone: " + sv.phone)
-    
+
     var reviewCount = $("<h5>")
     reviewCount.text("Reviews: " + sv.reviewCount)
     var button = $("<button>")
@@ -218,63 +222,66 @@ function createContent(sv){
     mapButton.text("Click for the Map")
     mapButton.data("latitude", sv.latitude)
     mapButton.data("longitude", sv.longitude)
-    var reviewButton=$("<button>")
-    reviewButton.data("id",sv.id)
+    var reviewButton = $("<button>")
+    reviewButton.data("id", sv.id)
     reviewButton.attr("data-target", ".moreImages")
     reviewButton.addClass("btn btn-secondary mx-2 my-2")
     reviewButton.addClass("clickReview")
     reviewButton.attr("data-toggle", "modal")
     reviewButton.text("Reviews")
-    
-      //using child added and snapshot fucntion to retrieve business names, reviews, pictures through pressing buttons
+
+    //using child added and snapshot fucntion to retrieve business names, reviews, pictures through pressing buttons
     //added buttons to get map location and click for map and added variables to place tattoo shop information on page
 
-    ul.append(name,stars, address, address2, phone, reviewCount, button, mapButton,reviewButton)
+    ul.append(name, stars, address, address2, phone, reviewCount, button, mapButton, reviewButton)
     row.append(image, ul)
     row.appendTo("#resultsDiv")
 }
 
 
 
-function reviewStars(ratings,stars){
+function reviewStars(ratings, stars) {
     console.log(ratings)
-    if(ratings.ratings===1.0){
-    stars.attr("src","../Project1/yelpStars/small_1.png")
-    }else if(ratings.ratings===1.5){
-    stars.attr("src","../Project1/yelpStars/small_1_half.png")
-    }else if(ratings.ratings===2.0){
-        stars.attr("src","../Project1/yelpStars/small_2.png")
-    }else if(ratings.ratings===2.5){
-        stars.attr("src","../Project1/yelpStars/small_2_half.png")
-    }else if(ratings.ratings===3.0){
-        stars.attr("src","../Project1/yelpStars/small_3.png")
-    }else if(ratings.ratings===3.5){
-        stars.attr("src","../Project1/yelpStars/small_3_half.png")
-    }else if(ratings.ratings===4.0){
-        stars.attr("src","../Project1/yelpStars/small_4.png")
-    }else if(ratings.ratings===4.5){
-        stars.attr("src","../Project1/yelpStars/small_4_half.png")
-    }else if(ratings.ratings===5.0){
-        stars.attr("src","../Project1/yelpStars/small_5.png")
+    if (ratings.ratings === 1.0) {
+        stars.attr("src", "../Project1/yelpStars/small_1.png")
+    } else if (ratings.ratings === 1.5) {
+        stars.attr("src", "../Project1/yelpStars/small_1_half.png")
+    } else if (ratings.ratings === 2.0) {
+        stars.attr("src", "../Project1/yelpStars/small_2.png")
+    } else if (ratings.ratings === 2.5) {
+        stars.attr("src", "../Project1/yelpStars/small_2_half.png")
+    } else if (ratings.ratings === 3.0) {
+        stars.attr("src", "../Project1/yelpStars/small_3.png")
+    } else if (ratings.ratings === 3.5) {
+        stars.attr("src", "../Project1/yelpStars/small_3_half.png")
+    } else if (ratings.ratings === 4.0) {
+        stars.attr("src", "../Project1/yelpStars/small_4.png")
+    } else if (ratings.ratings === 4.5) {
+        stars.attr("src", "../Project1/yelpStars/small_4_half.png")
+    } else if (ratings.ratings === 5.0) {
+        stars.attr("src", "../Project1/yelpStars/small_5.png")
     }
 
 }
 
-function rowExplanation(){
-    
+function rowExplanation() {
+
     var rowExplanation = $("<div>")
     rowExplanation.addClass("container")
     rowExplanation.addClass("h1")
     rowExplanation.addClass("my-5")
-    rowExplanation.css({"text-decoration":"underline"})
-    var tattooData=$("#tattooInput").find('option:selected').attr('id')
+    rowExplanation.addClass("explanation")
+    rowExplanation.css({
+        "text-decoration": "underline"
+    })
+    var tattooData = $("#tattooInput").find('option:selected').attr('id')
     console.log(tattooData)
-    rowExplanation.text("Shops that cater to " +tattooData+ " style tattoos")
+    rowExplanation.text("Shops that cater to " + tattooData + " style tattoos")
     rowExplanation.prependTo("#resultsDiv")
 }
 
-function imageError(){
+function imageError() {
     $("img").on("error", function () {
         $(this).attr("src", "../Project1/images/placeholder.png");
-      })
+    })
 }
--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 $(document).ready(function () {
-   
-    anime({
-        targets:'div.logo',
-        translateX:[
-            {value:1200, duration:3000},
-            {value:0, duration: 1000}
-        ],
-        rotate:{
-            value:'1turn',
-            easing: 'easeInOutSine',
-            delay:1000
-        },   
-    });
-     //animation of the tattoo4u symbol on the page
 
-     //putting our functions into place as the page loads to make sure that the elements are shown and hidden
+    anime({
+        targets: 'div.logo',
+        translateX: [{
+                value: 1200,
+                duration: 3000
+            },
+            {
+                value: 0,
+                duration: 1000
+            }
+        ],
+        rotate: {
+            value: '1turn',
+            easing: 'easeInOutSine',
+            delay: 1000
+        },
+    });
+    //animation of the tattoo4u symbol on the page
+
+    //putting our functions into place as the page loads to make sure that the elements are shown and hidden
     load();
     start();
     citySelect();
     back();
     finalSelection();
     back2(event);
-   
+
     //when the Back button is clicked from 2nd "page"
     function back() {
         $("#goBack").on("click", function () {
@@ -41,14 +46,20 @@ $(document).ready(function () {
     function start() {
         $("#start").on("click", function () {
             anime({
-                targets:'div.studentName',
-                translateY:[
-                    {value:-25,duration:1000},
-                    {value:0,duration:1000}, 
+                targets: 'div.studentName',
+                translateY: [{
+                        value: -25,
+                        duration: 1000
+                    },
+                    {
+                        value: 0,
+                        duration: 1000
+                    },
                 ],
-                delay: function(el, i, l) {
+                delay: function (el, i, l) {
                     return i * 200;
-             } })
+                }
+            })
             $("#intro").hide("fast");
             $("#area").fadeIn("slow");
             $("#body").css({
@@ -70,15 +81,15 @@ $(document).ready(function () {
     // after city is selected and Submit clicked
     function citySelect() {
         $("#citySubmit").on("click", function () {
-            if($("#city").val()==="Shop Location"){
+            if ($("#city").val() === "Shop Location") {
                 $("#citySubmit").attr("data-target", ".moreImages")
                 $("#citySubmit").attr("data-toggle", "modal")
-                var denied=$("<h3>")
+                var denied = $("<h3>")
                 denied.text("Make a selection to continue!")
                 denied.appendTo("#modalRow")
-            return
-            }else    
-            $("#modalRow").empty();
+                return
+            } else
+                $("#modalRow").empty();
             $("#citySubmit").removeAttr("data-target")
             $("#citySubmit").removeAttr("data-toggle")
             $("#area").hide("fast");
@@ -88,9 +99,9 @@ $(document).ready(function () {
             $("#body").css({
                 "background": "url(../Project1/images/backgroundShop2.jpg)"
             })
-         
+
         })
-    
+
 
     }
 
@@ -98,15 +109,15 @@ $(document).ready(function () {
     function finalSelection() {
         $("#tatSubmit").on("click", function () {
             console.log($("#tattooInput").val())
-            if($("#tattooInput").val()==="Which tattoo style did you decide on?"){
-            $("#tatSubmit").attr("data-target", ".moreImages")
-            $("#tatSubmit").attr("data-toggle", "modal")
-            var denied=$("<h3>")
-            denied.text("Make a selection to continue!")
-            denied.appendTo("#modalRow")
+            if ($("#tattooInput").val() === "Which tattoo style did you decide on?") {
+                $("#tatSubmit").attr("data-target", ".moreImages")
+                $("#tatSubmit").attr("data-toggle", "modal")
+                var denied = $("<h3>")
+                denied.text("Make a selection to continue!")
+                denied.appendTo("#modalRow")
                 return;
-            }else
-            $("#modalRow").empty();
+            } else
+                $("#modalRow").empty();
             $("#tatSubmit").removeAttr("data-target")
             $("#tatSubmit").removeAttr("data-toggle")
             $("#area").hide("fast");
@@ -114,12 +125,14 @@ $(document).ready(function () {
             $("#imageParagraph").hide("fast");
             $("#lastInput").hide("fast");
             cityValue();
-            $("#body").css({"background": "url(../Project1/images/backgroundInk.jpg)"})
+            $("#body").css({
+                "background": "url(../Project1/images/backgroundInk.jpg)"
+            })
             $("#newSearch").delay(3000).fadeIn(0);
-           $("#newSearch").on("click",function(){
-            reload();
-           })
-            
+            $("#newSearch").on("click", function () {
+                reload();
+            })
+
         })
     }
 
@@ -140,14 +153,18 @@ $(document).ready(function () {
     }
 
     // city and style are stored and pulled from database
-    function cityValue(){
-        var cityValue=$("#city").val();
-        var tattooStyle=$("#tattooInput").val();
-        pull(cityValue,tattooStyle)
+    function cityValue() {
+        var cityValue = $("#city").val();
+        var tattooStyle = $("#tattooInput").val();
+        pull(cityValue, tattooStyle)
         console.log(cityValue)
     }
-    function reload(){
+
+    function reload() {
         location.reload();
     }
 })
 
+window.onpopstate = function() {
+    alert("clicked back button");
+ }; 
